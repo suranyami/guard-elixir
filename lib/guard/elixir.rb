e13d69d96c39517a47057ed9f5771741ea87bc43
David Parry <david.parry@suranyami.com>
2014-11-26 16:46:37 +1100

    force color output

--- lib/guard/elixir.rb
+++ lib/guard/elixir.rb
@@ -35,7 +35,7 @@ module Guard
       total    = 0
       failures = 0
       duration = 0
-      run_command("mix test #{paths.join(' ')}") do |line|
+      run_command("mix test --color #{paths.join(' ')}") do |line|
         puts line
         if /Finished in ([0-9.]+) seconds/.match(line)
           duration = Regexp::last_match[1]
